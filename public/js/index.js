7e4d5608bca8ed6ee214dc7a37a2abb4aab6517d
Ragnorok303 <jasontompkins22@gmail.com>
2020-05-12 12:43:58 -0600

    debugged

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
   $(document).on("click", "button.login", signin);
-  
+
   var signin = function(credentials) {
     //Sign in user with a account
     event.preventDefault();
     var userinputname = $("#userName").val().trim();
     var userpassword = $("#password").val().trim();
-    if (userinputname === username) { 
+    if (userinputname === username) {
       userinputname = "/name/" + userinputname;
     }
     $.get("/api/newuser" + userinputname, function(data) {
       console.log("User", data);
-      if (userpassword === password){
+      if (userpassword === password) {
         userHome();
-      }
-      else {
+      } else {
         alert("Username or Password incorrect");
       }
     });
