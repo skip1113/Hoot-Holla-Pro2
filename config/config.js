a420ca62a577bff5af4c12b205ab29e935d57203
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-05-13 14:45:14 -0600

    added hoot.js file and changes references to json

---
+++ config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "testdb",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    // eslint-disable-next-line camelcase
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
