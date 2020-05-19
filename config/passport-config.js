82bb406497570f425680b633e844131b987c1701
Ragnorok303 <jasontompkins22@gmail.com>
2020-05-18 20:02:00 -0600

    heroku debugging

--- config/passport-config.js
+++ config/passport-config.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "rootroot",
+    password: "R@gnorok303B@man",
     database: "hoot_holla"
   });
   connection.connect(function(err) {
